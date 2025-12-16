c42f4c4a4b97e46725896e791ce0d8d1f1d4b249
Akiiila11 <akilavenkatraj2001@gmail.com>
2025-12-16 15:41:28 +0530

    Updated

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -6,7 +6,7 @@ const Projects = () => {
   return (
     <section id='projects' className='w-full h-full flex flex-col items-center justify-center px-4 md:px-6 py-40 dark:bg-linear-to-r dark:from-black dark:to-black'>
       <h1 className='font-bold text-4xl md:text-6xl text-center dark:text-white'>My Crafts</h1>
-      <p className='mt-3 dark:text-gray-600 text-gray-700 text-center text-sm'>The following projects demonstrate my hands-on experience with modern web technologies, including brief descriptions and tech stacks.</p>
+      <p className='mt-3 dark:text-gray-600 text-gray-700 text-center text-sm md:text-lg'>The following projects demonstrate my hands-on experience with modern web technologies, including brief descriptions and tech stacks.</p>
       <div className='max-w-7xl w-full h-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-15'>
         {projects.map((proj)=> (
           <div key={proj.id} className=' w-full h-full shadow-2xl dark:shadow-white/50 px-3 md:px-5 py-4 md:py-10
